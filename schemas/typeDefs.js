86470080f18cd41c28458dba46f127081d497062
Nicholas Easter <nicholas.easter17@gmail.com>
2022-04-09 10:02:01 -0700

    finished query mutations for adding thoughts reactions and friends as well as added authentication to tell when the user is logged in

--- schemas/typeDefs.js
+++ schemas/typeDefs.js
@@ -1,5 +1,6 @@
 // import the gql tagged template function
 const { gql } = require('apollo-server-express');
+const { signToken } = require('../utils/auth');
 
 
 
@@ -31,11 +32,27 @@ const typeDefs = gql`
     }
 
     type Query {
+        me: User
         users: [User],
         user(username: String!): User
         thoughts(username: String): [Thought]
         thought(_id: ID!): Thought
-      }`;
+      }
+      
+    type Mutation {
+        login(email: String!, password: String!): Auth
+        addUser(username: String!, email: String!, password: String!): Auth
+        addThought(thoughtText: String!): Thought
+        addReaction(thoughtId: ID!, reactionBody: String!): Thought
+        addFriend(friendId: ID!): User
+    }
+
+    type Auth {
+        token: ID!
+        user: User
+    }
+      
+      `;
 
 // export the typeDefs
 module.exports = typeDefs;
